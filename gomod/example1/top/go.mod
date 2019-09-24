32263ee532440e91078a7f018a80b4b47ed34aa8
mrchar <51408183+mrchar@users.noreply.github.com>
2019-09-24 17:57:09 +0800

    添加top包

---
+++ gomod/example1/top/go.mod
@@ -0,0 +1,5 @@
+module github.com/mrchar/go-example/example1/top
+
+go 1.13
+
+require github.com/mrchar/go-example v0.0.0-20190924094906-b59b8e0d6b99
---
+++ gomod/example1/top/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mrchar/go-example/gomod/example1/sub"
+)
+
+func main(){
+	fmt.Printf("%#v\n", sub.A)
+}
